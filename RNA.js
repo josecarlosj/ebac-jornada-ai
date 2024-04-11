f2245a1b0c8b4e773be29bb65939cf88e575dd6e
josecarlosj <junior.edf@gmail.com>
2024-04-11 16:22:02 -0300

    start implementing RNA script

---
+++ RNA.js
@@ -0,0 +1,38 @@
+// RNA - Rede Neural Artificial
+
+function randomRange(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function lerp(a, b, t) {
+    return a + (b - a) * t
+}
+
+class Neuron {
+    constructor(inputs) {
+        this.bias = randomRange(-1, 1)
+
+        this.weightList = new Array(inputs)
+        .fill()
+        .map(() => randomRange(-1, 1))
+    }
+}
+
+g(signalList = []); {
+    let u = 0;
+
+    for (let i = 0; i < this.weightList.lenght; i++) {
+        u += signalList[i] * this.weightList[i]
+    }
+
+    if (Math.tanh(u) > this.bias) return 1 // Neuron ativado
+    else return 0; // Neuron desativado
+}
+
+mutate(rate = 1); {
+    this.weightList = this.weightList.map(() => {
+        return lerp(w, randomRange(-1, 1), rate)
+    });
+
+    this.bias = lerp(this.bias, randomRange(-1, 1), range)
+}
