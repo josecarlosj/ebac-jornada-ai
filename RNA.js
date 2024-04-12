9f771297ea5077f6a0918f42b6452a370449c4d9
josecarlosj <junior.edf@gmail.com>
2024-04-12 14:58:50 -0300

    update RNA.js and create utils.js

--- RNA.js
+++ RNA.js
@@ -36,3 +36,57 @@ mutate(rate = 1); {
 
     this.bias = lerp(this.bias, randomRange(-1, 1), range)
 }
+
+class RNA {
+    constructor(inputCount = 1, levelList = []) {
+        this.score = 0;
+
+        this.levelList = levelList.map((l, i) => {
+            const inputSize = i === 0 ? inputCount : levelList[i - 1];
+
+            return new Array(l).fill().map(() => new Neuron(inputSize));
+        })
+    }
+
+    compute(list = []) {
+        for (let i = 0; i < this.levelList.length; i++) {
+            const tempList = []
+
+            for (const neuron of this.levelList[i]) {
+                if (list.length !== neuron.weightList.length) throw new Error("Entrada inválida");
+                tempList.push(neuron.g(list))
+            }
+            list = tempList;
+        }
+        return list;
+    }
+}
+
+mutate(rate = 1); {
+    for (const level of this.levelList) {
+        for (const neuron of level) neuron.mutate(rate)
+    }
+}
+
+load(rna); {
+    if (!rna) return
+    try {
+        this.levelList = rna.map((neuronList) => {
+            return neuronList.map((neuron) => {
+                const n = new Neuron();
+                n.bias = neuron.bias;
+                n.weightList = neuron.weightList;
+
+                return n;
+            });
+        });
+    } catch (e) {
+        return;
+    }
+
+    save(); {
+        return this.levelList;
+    }
+}
+
+export default RNA;
